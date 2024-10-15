86ef9d8a7d051cb60638c611333fcff1858c8e48
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-15 15:07:51 +0200

    add: for loop

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 for (let i=0;i < 100;i++){
+    let num = i + 1
+    let multiplo3 = num % 3
+    let multiplo5 = num % 5
+    if (multiplo3 ===0 && multiplo5 === 0) {
+        console.log('FizzBuzz')
+    } else if (multiplo3 === 0){
+        console.log('Fizz')
+    } else if (multiplo5 === 0){
+        console.log('Buzz')
+    } else {
+        console.log(num)
+    }
+     
+     
     
 }
+  
